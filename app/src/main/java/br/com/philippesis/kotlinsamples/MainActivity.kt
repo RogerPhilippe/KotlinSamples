24a4f8a1ab43f44482f1702e1a9ff9ab6ba5bd0c
Roger Philippe Pereira <rogerphilippe@outlook.com>
2019-06-21 18:15:17 -0300

    Initial commit

---
+++ app/src/main/java/br/com/philippesis/kotlinsamples/MainActivity.kt
@@ -0,0 +1,45 @@
+package br.com.philippesis.kotlinsamples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var txtSample: TextView
+    private lateinit var btnSample: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initComps()
+        setupListenerComps()
+
+    }
+
+    private fun initComps() {
+
+        this.txtSample = findViewById(R.id.txtSample)
+        this.btnSample = findViewById(R.id.btnSample)
+
+    }
+
+    private fun setupListenerComps() {
+
+        this.btnSample.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+
+        if (v == btnSample) {
+            this.txtSample.text = "Olá Mundo!"
+        }
+
+    }
+
+}
